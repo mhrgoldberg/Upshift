4f2ae5c2508695f176a7a00136a3f1204e37033c
mitchellreiss <mitchellreiss@gmail.com>
2019-11-15 16:48:47 -0800

    Added directions functionality to map

--- frontend/components/route/create_route.jsx
+++ frontend/components/route/create_route.jsx
@@ -4,9 +4,13 @@ class CreateRoute extends React.Component {
 
   constructor(props) {
     super(props);
-    this.markerTrack = [];
+    this.state = {
+      workout_mode: "WALKING",
+      markerTrack: []
+    }
     this.addMarker = this.addMarker.bind(this);
-    this.drawPolyline = this.drawPolyline.bind(this);
+    this.calcRoute = this.calcRoute.bind(this);
+    // this.drawPolyline = this.drawPolyline.bind(this);
   }
 
   componentDidMount() {
@@ -14,43 +18,51 @@ class CreateRoute extends React.Component {
       center: { lat: 37.7758, lng: -122.435 }, // this is SF
       zoom: 15
     };
-
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
     this.map = new google.maps.Map(this.refs.map, mapOptions);
-
+    this.directionsRenderer.setMap(this.map);
     google.maps.event.addListener(this.map, 'click', (e) => {
       this.addMarker(e.latLng);
+      this.calcRoute()
     });
+  }
+
+  calcRoute() {
+    const request = {
+      origin: this.state.markerTrack[0].position,
+      destination: this.state.markerTrack[this.state.markerTrack.length-1].position,
+      travelMode: this.state.workout_mode,
+      avoidFerries: true,
+      avoidHighways: true,
+      avoidTolls: true,
+    };
+
+    this.directionsService.route(request, (result, status) => {
+      if (status == 'OK') {
+        this.directionsRenderer.setDirections(result);
+      }
+    })
   }
 
   addMarker(latLng) {
     let marker = new google.maps.Marker({
         map: this.map,
         position: latLng,
-        draggable: true
     });
-    this.markerTrack.push(marker);
-    // marker.addListener('position_changed', function() {
-    //   drawPolyline();
-    // });
-    //store the marker object drawn in global array
+    let markerTrack = [...this.state.markerTrack];
+    markerTrack.push(marker);
+    this.setState({ markerTrack });
   }
 
-  drawPolyline() {
-    let markersPositionArray = [];
-    // obtain latlng of all markers on map
-    markersArray.forEach(function(e) {
-      markersPositionArray.push(e.getPosition());
-    });
-    // draw new polyline at markers' position
-    polyline = new google.maps.Polyline({
-      map: map,
-      path: markersPositionArray,
-      strokeOpacity: 0.4
-    });
-  }
-
-  // componentDidUpdate() {
-  //   this.MarkerManager.updateMarkers(this.props.benches);
+  // drawPolyline() {
+  //   let road = new google.maps.Polyline({
+  //     path: this.markerTrack,
+  //     strokeColor: "#FF0000",
+  //     strokeOpacity: 1,
+  //     strokeWeight: 2
+  //   });
+  //   road.setMap(this.map);
   // }
 
   render() {
