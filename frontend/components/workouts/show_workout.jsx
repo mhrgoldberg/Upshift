604408f8887f9b9c20a558f2d384837e13f013f9
mitchellreiss <mitchellreiss@gmail.com>
2019-12-11 09:40:48 -0800

    Update workout show styling

--- frontend/components/workouts/show_workout.jsx
+++ frontend/components/workouts/show_workout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 class ShowWorkout extends React.Component {
   constructor(props) {
     super(props);
@@ -15,7 +15,7 @@ class ShowWorkout extends React.Component {
     this.directionsService = new google.maps.DirectionsService();
     this.directionsRenderer = new google.maps.DirectionsRenderer();
     this.elevationService = new google.maps.ElevationService();
-  
+
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
       zoom: 15
@@ -42,7 +42,7 @@ class ShowWorkout extends React.Component {
           quality = payload.workout.quality;
         }
         const data = [fatigue, mood, motivation, quality];
-        new Chart(myChartRef, {
+        const polarChart = new Chart(myChartRef, {
           type: "polarArea",
           data: {
             labels: ["fatigue", "mood", "motivation", "quality"],
@@ -50,7 +50,12 @@ class ShowWorkout extends React.Component {
               {
                 label: "subjective workout paramaters",
                 data,
-                backgroundColor: ["#fd4c01c4", "#007fb6c4", "#c6eac6c4", "#ea4335c4"]
+                backgroundColor: [
+                  "#fd4c01c4",
+                  "#007fb6c4",
+                  "#c6eac6c4",
+                  "#ea4335c4"
+                ]
               }
             ]
           },
@@ -77,31 +82,31 @@ class ShowWorkout extends React.Component {
         markers.forEach(marker => bounds.extend(marker));
         this.map.setCenter(bounds.getCenter());
         this.map.fitBounds(bounds);
-        
+
         const request = {
           origin: routeData[0].location,
-          destination: routeData[routeData.length-1].location,
+          destination: routeData[routeData.length - 1].location,
           waypoints: routeData.slice(1, -1),
-          travelMode: payload.route.route_type === 'Running' ? 'WALKING' : 'BICYCLING',
+          travelMode:
+            payload.route.route_type === "Running" ? "WALKING" : "BICYCLING",
           optimizeWaypoints: false,
           avoidFerries: true,
           avoidHighways: true,
-          avoidTolls: true,
+          avoidTolls: true
         };
-    
+
         this.directionsService.route(request, (result, status) => {
-          if (status == 'OK') {   
+          if (status == "OK") {
             this.directionsRenderer.setDirections(result);
           }
         });
-        const latLng = routeData.map( point =>  { 
-          return ({
-            lat: point.location.lat, 
+        const latLng = routeData.map(point => {
+          return {
+            lat: point.location.lat,
             lng: point.location.lng
-          })
+          };
         });
         // this.getElevationSample(latLng, this.elevationService)
-
       });
   }
   // getElevationSample(path, elevator) {
@@ -127,7 +132,7 @@ class ShowWorkout extends React.Component {
 
   //   // window.google.load('visualization', '1', {packages: ['columnchart']});
   //   this.chart = new google.visualization.ColumnChart(chartDiv);
-    
+
   //   var data = new google.visualization.DataTable();
   //   data.addColumn('string', 'Sample');
   //   data.addColumn('number', 'Elevation');
@@ -143,75 +148,103 @@ class ShowWorkout extends React.Component {
   // }
 
   render() {
-    const {workout_type, avg_speed, avg_hr, comment, title, duration, resting_hr, id} = this.props.workouts;
-    const {distance, elevation_gain, elevation_loss, max_elevation} = this.props.routes;
-    const hours = Math.floor(duration/60);
-    const minutes = ("0" + Math.floor(duration%60)).slice(-2);
+    const {
+      workout_type,
+      avg_speed,
+      avg_hr,
+      comment,
+      title,
+      duration,
+      resting_hr,
+      id
+    } = this.props.workouts;
+    const {
+      distance,
+      elevation_gain,
+      elevation_loss,
+      max_elevation
+    } = this.props.routes;
+    const hours = Math.floor(duration / 60);
+    const minutes = ("0" + Math.floor(duration % 60)).slice(-2);
     const seconds = ("0" + Math.round((duration % 1) * 60)).slice(-2);
-    const icon =  workout_type === "Cycling" ? <i className="fas fa-biking"></i> : <i className="fas fa-running"></i>
+    const icon =
+      workout_type === "Cycling" ? (
+        <i className="fas fa-biking"></i>
+      ) : (
+        <i className="fas fa-running"></i>
+      );
 
     return (
       <div className="show-workout-container">
         <div className="routes-sub-header">
-          <h1>{icon} {title}</h1>
+          <h1>
+            {icon} {title}
+          </h1>
           <div className="workout-header-buttons">
-          <Link to="/workouts"><button>All Workouts</button></Link>
-          <Link to={`/workout/edit/${id}`}><button>Edit Workout</button></Link>
+            <Link to="/workouts">
+              <button>All Workouts</button>
+            </Link>
+            <Link to={`/workout/edit/${id}`}>
+              <button>Edit Workout</button>
+            </Link>
           </div>
         </div>
-
-        <div className="workout-show-data">
+        <div className="workout-border">
+          <div className="workout-show-data">
             <ul>
-            <li key="Duration">
-              <div className="data">{hours}:{minutes}:{seconds}</div>
-              <div className="data-title">Duration</div>
-            </li>
-            <li key="dist">
-              <div className="data">{distance.toFixed(2)} mi</div>
-              <div className="data-title">Distance</div>
-            </li>
-            <li key="avg-speed">
-              <div className="data">{avg_speed.toFixed(2)} mph</div>
-              <div className="data-title">Avg Speed</div>
-            </li>
-            <li key="gain">
-              <div className="data">{elevation_gain} ft</div>
-              <div className="data-title">Elevation Gain</div>
-            </li>
-            <li key="loss">
-              <div className="data">{elevation_loss} ft</div>
-              <div className="data-title">Elevation Loss</div>
-            </li>
-            <li key="max">
-              <div className="data">{max_elevation} ft</div>
-              <div className="data-title">Max Elevation</div>
-            </li>
-            <li key="avg-hr">
-              <div className="data">{avg_hr} bpm</div>
-              <div className="data-title">Avg HR</div>
-            </li>
-            <li key="resting-hr">
-              <div className="data">{resting_hr} bpm</div>
-              <div className="data-title">Resting HR</div>
-            </li>
-          </ul>
-        </div>
-        <div className="workout-row">
-          <div className="workout-map">
-            <div id="map" ref='map' />
-            <div id="elevation_chart"></div>
+              <li key="Duration">
+                <div className="data">
+                  {hours}:{minutes}:{seconds}
+                </div>
+                <div className="data-title">Duration</div>
+              </li>
+              <li key="dist">
+                <div className="data">{distance.toFixed(2)} mi</div>
+                <div className="data-title">Distance</div>
+              </li>
+              <li key="avg-speed">
+                <div className="data">{avg_speed.toFixed(2)} mph</div>
+                <div className="data-title">Avg Speed</div>
+              </li>
+              <li key="gain">
+                <div className="data">{elevation_gain} ft</div>
+                <div className="data-title">Elevation Gain</div>
+              </li>
+              <li key="loss">
+                <div className="data">{elevation_loss} ft</div>
+                <div className="data-title">Elevation Loss</div>
+              </li>
+              <li key="max">
+                <div className="data">{max_elevation} ft</div>
+                <div className="data-title">Max Elevation</div>
+              </li>
+              <li key="avg-hr">
+                <div className="data">{avg_hr} bpm</div>
+                <div className="data-title">Avg HR</div>
+              </li>
+              <li key="resting-hr">
+                <div className="data">{resting_hr} bpm</div>
+                <div className="data-title">Resting HR</div>
+              </li>
+            </ul>
           </div>
-        
-          <div className="subjective-polar-chart">
-            <canvas id="myChart" ref={this.chartRef} />
-            <h3>Comment</h3>
-            <div className="workout-comment"><p>{comment}</p></div>
+          <div className="workout-row">
+            <div className="workout-map">
+              <div id="map" ref="map" />
+              <div id="elevation_chart"></div>
+            </div>
+
+            <div className="subjective-polar-chart">
+              <canvas id="myChart" ref={this.chartRef} />
+              <h3>Comment</h3>
+              <div className="workout-comment">
+                <p>{comment}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
-
-
   }
 }
 
