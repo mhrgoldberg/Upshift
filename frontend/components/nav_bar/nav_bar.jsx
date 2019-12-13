2ea2988d6370a6cd98752348e45e1de8cfaeac97
mitchellreiss <mitchellreiss@gmail.com>
2019-12-12 17:14:18 -0800

    Update feed links

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -22,7 +22,7 @@ class NavBar extends React.Component {
   loggedInBar() {
     return (
       <div className="r-side-buttons">
-        <div className="profile_pic"></div>
+        <div className="profile-pic"></div>
         <button onClick={this.props.logout}>logout</button>
       </div>
     )
